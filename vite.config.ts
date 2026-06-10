31a825cfa2a78f770c4bd7b77d0b64fb33324122
aziz-boujaada <azizboujaada87@gmail.com>
2026-06-10 10:33:34 +0100

    fix : fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: '/NectAct-Ecom-front/',
+
   server: {
     host: '127.0.0.1',
     port: 5173,
