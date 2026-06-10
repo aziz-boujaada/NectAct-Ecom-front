b66d4e9dec2acb08bbc630cb067728e3330b4df4
aziz-boujaada <azizboujaada87@gmail.com>
2026-06-10 10:51:33 +0100

    fix translate errors in prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/NectAct-Ecom-front/',
+  base: import.meta.env.BASE_URL + '/NectAct-Ecom-front/',
 
   server: {
     host: '127.0.0.1',
